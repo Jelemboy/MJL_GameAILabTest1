4dabf567f4388e5a3fc85ffdffbf050b72a80b73
Jelemboy <jelemboy@gmail.com>
2014-03-10 21:05:27 +0000

    Test incomplete
    Did not finish, put as much correct code as I could in

---
+++ Assets/CircleFollowing.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CircleFollowing : MonoBehaviour
+{
+
+	public float mass = 1.0f;
+	public float maxSpeed = 5.0f;
+
+	public Vector3 velocity;
+	public Vector3 force;
+
+	public int currentWaypoint;
+	public int lastWaypoint;
+
+	public List<Vector3> wayPoints = new List<Vector3>();
+
+	private void CreatePath(int points, float radius)
+	{
+		wayPoints.Clear();
+
+		float thetaInc = (2*Mathf.PI)/points;
+		for(int point = 0; point < points; point ++)
+		{
+			float lastX = 0;
+			float lastY = radius;
+			float theta = (thetaInc * point)/2;
+			//wayPoints.x = Mathf.Sin(theta) * radius;
+			//wayPoints.z = - Mathf.Cos(theta) * radius;
+
+		}	
+	}
+
+	private void DrawPath()
+	{
+		for(i = 1; i < wayPoints.Count(); i ++)
+		{
+			Debug.DrawLine(wayPoints[i - 1], wayPoints[i], Color.red);
+		}
+	}
+
+	Vector3 Seek(Vector3 point)
+	{
+		Vector3 desired = point - transform.position;
+		desired.Normalize();
+		desired *= maxSpeed;
+		return(desired - velocity);
+	}
+
+	Vector3 FollowPath()
+	{
+		Vector3 toCurrent = wayPoints[currentWaypoint] - transform.position;
+		float distanceToCurrent = toCurrent.magnitude;
+
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		CreatePath(10,10.0f);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
---
+++ Assets/CubeArrive.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeArrive : MonoBehaviour {
+
+	public Vector3 velocity;
+	public Vector3 force;
+
+	public float maxSpeed = 5.0f;
+	public float mass = 1.0f;
+
+	public Vector3 target;
+
+	Vector3 ArriveAtTarget()
+	{
+		float slowingRange = 10.0f;
+		Vector3 toTarget = target - transform.position;
+		float distanceToTarget = toTarget.magnitude;
+		if (distanceToTarget < 1)
+		{
+			target = Random.insideUnitCircle*Random.Range(0,9);
+		}
+			toTarget.Normalize();
+			toTarget *= maxSpeed;
+			return(desired - velocity);
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		target = Random.insideUnitCircle*Random.Range(0,9);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
